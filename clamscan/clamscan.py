b8f5c251333f4b7187c909e08f97bc7e7dcc107a
Chema Garcia <chema@safetybits.net>
2017-06-15 15:39:25 +0200

    testing

---
+++ clamscan/clamscan.py
@@ -0,0 +1,66 @@
+from fame.core.module import ProcessingModule
+from fame.common.exceptions import ModuleInitializationError
+
+try:
+    import pyclamd
+
+    HAVE_CLAMD = True
+except:
+    HAVE_CLAMD = False
+
+class Clamscan(ProcessingModule):
+    name = "clamscan"
+    description = "Scan files with ClamAV"
+
+    config = [
+        {
+            'name': 'filename',
+            'type': 'str',
+            'description': 'UNIX socket file path'
+        },
+        {
+            'name': 'server',
+            'type': 'str',
+            'description': 'ClamAV server'
+        },
+        {
+            'name': 'port',
+            'type': 'str',
+            'description': 'ClamAV port'
+        }
+    ]
+
+    _clam = None
+    _tag = 'malware'
+
+    def initialize(self):
+        if not HAVE_CLAMD:
+            raise ModuleInitializationError(self, "Missing dependency: pyclamd")
+
+
+    def each(self, target):
+        if self._clam is None:
+            if len(self.filename) > 0:
+                self._clam = pyclamd.ClamdUnixSocket(filename="/var/run/clamav/clamd.ctl")
+            elif len(self.server) > 0 and len(self.port) > 0:
+                try:
+                    host = self.server.strip()
+                    port = int(self.port.strip())
+                    self._clam = pyclamd.ClamdNetworkSocket(host=host,port=port)
+                except:
+                    return False
+            else:
+                return False
+
+            if not self._clam.ping():
+                return False
+
+        res = self._clam.scan_file(target)
+        if not res:
+            return True
+
+        status,name = res['stream']
+        self.add_tag(self._tag)
+        self.results = {'signature': [name]}
+
+        return True
