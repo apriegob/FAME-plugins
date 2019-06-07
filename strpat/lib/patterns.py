9c336d21c212ff70223bf1cc6c34be8b94c16ac4
nv3n0m <apriegob@gmail.com>
2019-06-07 15:11:47 +0200

    updated strings plugin with suspicious detection

--- strpat/lib/patterns.py
+++ strpat/lib/patterns.py
@@ -2,9 +2,8 @@ import re
 
 patterns = {
     "btc": re.compile(r'^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$'),
-    "domain": re.compile(r'^(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.(?!dll)[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$'),
+    "suspicious": re.compile(r'^(http|https):\/\/|[a-z0-9]{3,}([\-\.]{1}[a-z0-9]+)*\.(?!dll)[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$'),
     "ipaddress": re.compile(r'^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?:\/[0-9]{1,2})?')
-
 }
 
 
